2c16f6cca5dec952c0efb292b27fa0a04dd5d1cc
Arrokoth486958 <87651575+Arrokoth486958@users.noreply.github.com>
2021-07-20 21:28:01 +0200

    Renamed file

--- src/main/java/mod/sol/entities/boss/EntityUranusBossSlime.java
+++ src/main/java/mod/sol/entities/boss/EntityUranusBossSlime.java
@@ -3,6 +3,7 @@ package mod.sol.entities.boss;
 import javax.annotation.Nullable;
 
 import micdoodle8.mods.galacticraft.api.entity.IEntityBreathable;
+import micdoodle8.mods.galacticraft.core.entities.EntityBossBase;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
@@ -19,6 +20,7 @@ import net.minecraft.init.Items;
 import net.minecraft.init.MobEffects;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.network.datasync.DataSerializers;
@@ -31,33 +33,37 @@ import net.minecraft.util.SoundEvent;
 import net.minecraft.util.datafix.DataFixer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
-import net.minecraft.world.DifficultyInstance;
-import net.minecraft.world.EnumDifficulty;
-import net.minecraft.world.World;
-import net.minecraft.world.WorldType;
+import net.minecraft.world.*;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.storage.loot.LootTableList;
 
-public class EntityUranusBossSlime extends EntitySlime implements IEntityBreathable
+import java.util.Random;
+
+public class EntityUranusBossSlime extends EntityBossBase implements IEntityBreathable
 {
     public EntityUranusBossSlime(World worldIn) {
         super(worldIn);
     }
 
-    @Nullable
-    public IEntityLivingData onInitialSpawn(DifficultyInstance difficulty, @Nullable IEntityLivingData livingdata)
-    {
-        int i = this.rand.nextInt(3);
+    @Override
+    public int getChestTier() {
+        return 0;
+    }
 
-        if (i < 2 && this.rand.nextFloat() < 0.5F * difficulty.getClampedAdditionalDifficulty())
-        {
-            ++i;
-        }
+    @Override
+    public ItemStack getGuaranteedLoot(Random rand) {
+        return null;
+    }
 
-        int j = 1 << i;
-        this.setSlimeSize(j, true);
-        return super.onInitialSpawn(difficulty, livingdata);
+    @Override
+    public void dropKey() {
+
+    }
+
+    @Override
+    public BossInfo.Color getHealthBarColor() {
+        return null;
     }
 
     @Override
