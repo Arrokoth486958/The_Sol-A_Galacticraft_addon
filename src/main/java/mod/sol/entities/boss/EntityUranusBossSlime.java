48c482d4b567010bbfbbcc8bf8fd372c43ad4104
Arrokoth486958 <87651575+Arrokoth486958@users.noreply.github.com>
2021-07-20 21:56:53 +0200

    Updated tier 7 stuff and Uranus Slime boss

--- src/main/java/mod/sol/entities/boss/EntityUranusBossSlime.java
+++ src/main/java/mod/sol/entities/boss/EntityUranusBossSlime.java
@@ -1,69 +1,344 @@
 package mod.sol.entities.boss;
 
-import javax.annotation.Nullable;
 
+import micdoodle8.mods.galacticraft.api.GalacticraftRegistry;
 import micdoodle8.mods.galacticraft.api.entity.IEntityBreathable;
 import micdoodle8.mods.galacticraft.core.entities.EntityBossBase;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLiving;
+import micdoodle8.mods.galacticraft.core.util.ConfigManagerCore;
+import mod.sol.init.SolItems;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.IEntityLivingData;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.entity.ai.EntityAIFindEntityNearest;
 import net.minecraft.entity.ai.EntityAIFindEntityNearestPlayer;
 import net.minecraft.entity.ai.EntityMoveHelper;
-import net.minecraft.entity.monster.EntitySlime;
+import net.minecraft.entity.monster.EntityIronGolem;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Biomes;
-import net.minecraft.init.Items;
 import net.minecraft.init.MobEffects;
 import net.minecraft.init.SoundEvents;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.network.datasync.DataParameter;
-import net.minecraft.network.datasync.DataSerializers;
-import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.pathfinding.PathNavigateGround;
-import net.minecraft.util.DamageSource;
-import net.minecraft.util.EnumParticleTypes;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
-import net.minecraft.util.datafix.DataFixer;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.*;
-import net.minecraft.world.biome.Biome;
-import net.minecraft.world.chunk.Chunk;
-import net.minecraft.world.storage.loot.LootTableList;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 
 public class EntityUranusBossSlime extends EntityBossBase implements IEntityBreathable
 {
-    public EntityUranusBossSlime(World worldIn) {
+    protected Random rand;
+
+    public EntityUranusBossSlime(World worldIn)
+    {
         super(worldIn);
+        this.setSize(5, 5);
+        this.moveHelper = new EntityUranusBossSlime.SlimeMoveHelper(this);
+    }
+
+    protected void initEntityAI()
+    {
+        this.tasks.addTask(1, new EntityUranusBossSlime.AISlimeFloat(this));
+        this.tasks.addTask(2, new EntityUranusBossSlime.AISlimeAttack(this));
+        this.tasks.addTask(3, new EntityUranusBossSlime.AISlimeFaceRandom(this));
+        this.tasks.addTask(5, new EntityUranusBossSlime.AISlimeHop(this));
+        this.targetTasks.addTask(1, new EntityAIFindEntityNearestPlayer(this));
+        this.targetTasks.addTask(3, new EntityAIFindEntityNearest(this, EntityIronGolem.class));
+    }
+
+    protected void applyEntityAttributes()
+    {
+        super.applyEntityAttributes();
+        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(600.0D * ConfigManagerCore.dungeonBossHealthMod);
+    }
+
+    protected SoundEvent getJumpSound()
+    {
+        return SoundEvents.ENTITY_SLIME_JUMP;
+    }
+
+    protected boolean makesSoundOnJump()
+    {
+        return true;
+    }
+
+    protected int getJumpDelay()
+    {
+        return this.rand.nextInt(20) + 10;
+    }
+
+    static class AISlimeAttack extends EntityAIBase
+    {
+        private final EntityUranusBossSlime slime;
+        private int growTieredTimer;
+
+        public AISlimeAttack(EntityUranusBossSlime slimeIn)
+        {
+            this.slime = slimeIn;
+            this.setMutexBits(2);
+        }
+
+        /**
+         * Returns whether the EntityAIBase should begin execution.
+         */
+        public boolean shouldExecute()
+        {
+            EntityLivingBase entitylivingbase = this.slime.getAttackTarget();
+
+            if (entitylivingbase == null)
+            {
+                return false;
+            }
+            else if (!entitylivingbase.isEntityAlive())
+            {
+                return false;
+            }
+            else
+            {
+                return !(entitylivingbase instanceof EntityPlayer) || !((EntityPlayer)entitylivingbase).capabilities.disableDamage;
+            }
+        }
+
+        /**
+         * Execute a one shot task or start executing a continuous task
+         */
+        public void startExecuting()
+        {
+            this.growTieredTimer = 300;
+            super.startExecuting();
+        }
+
+        /**
+         * Returns whether an in-progress EntityAIBase should continue executing
+         */
+        public boolean shouldContinueExecuting()
+        {
+            EntityLivingBase entitylivingbase = this.slime.getAttackTarget();
+
+            if (entitylivingbase == null)
+            {
+                return false;
+            }
+            else if (!entitylivingbase.isEntityAlive())
+            {
+                return false;
+            }
+            else if (entitylivingbase instanceof EntityPlayer && ((EntityPlayer)entitylivingbase).capabilities.disableDamage)
+            {
+                return false;
+            }
+            else
+            {
+                return --this.growTieredTimer > 0;
+            }
+        }
+
+        /**
+         * Keep ticking a continuous task that has already been started
+         */
+        public void updateTask()
+        {
+            this.slime.faceEntity(this.slime.getAttackTarget(), 10.0F, 10.0F);
+            ((EntityUranusBossSlime.SlimeMoveHelper)this.slime.getMoveHelper()).setDirection(this.slime.rotationYaw, true);
+        }
+    }
+
+    static class AISlimeFaceRandom extends EntityAIBase
+    {
+        private final EntityUranusBossSlime slime;
+        private float chosenDegrees;
+        private int nextRandomizeTime;
+
+        public AISlimeFaceRandom(EntityUranusBossSlime slimeIn)
+        {
+            this.slime = slimeIn;
+            this.setMutexBits(2);
+        }
+
+        /**
+         * Returns whether the EntityAIBase should begin execution.
+         */
+        public boolean shouldExecute()
+        {
+            return this.slime.getAttackTarget() == null && (this.slime.onGround || this.slime.isInWater() || this.slime.isInLava() || this.slime.isPotionActive(MobEffects.LEVITATION));
+        }
+
+        /**
+         * Keep ticking a continuous task that has already been started
+         */
+        public void updateTask()
+        {
+            if (--this.nextRandomizeTime <= 0)
+            {
+                this.nextRandomizeTime = 40 + this.slime.getRNG().nextInt(60);
+                this.chosenDegrees = (float)this.slime.getRNG().nextInt(360);
+            }
+
+            ((EntityUranusBossSlime.SlimeMoveHelper)this.slime.getMoveHelper()).setDirection(this.chosenDegrees, false);
+        }
+    }
+
+    static class AISlimeFloat extends EntityAIBase
+    {
+        private final EntityUranusBossSlime slime;
+
+        public AISlimeFloat(EntityUranusBossSlime slimeIn)
+        {
+            this.slime = slimeIn;
+            this.setMutexBits(5);
+            ((PathNavigateGround)slimeIn.getNavigator()).setCanSwim(true);
+        }
+
+        /**
+         * Returns whether the EntityAIBase should begin execution.
+         */
+        public boolean shouldExecute()
+        {
+            return this.slime.isInWater() || this.slime.isInLava();
+        }
+
+        /**
+         * Keep ticking a continuous task that has already been started
+         */
+        public void updateTask()
+        {
+            if (this.slime.getRNG().nextFloat() < 0.8F)
+            {
+                this.slime.getJumpHelper().setJumping();
+            }
+
+            ((EntityUranusBossSlime.SlimeMoveHelper)this.slime.getMoveHelper()).setSpeed(1.2D);
+        }
+    }
+
+    static class AISlimeHop extends EntityAIBase
+    {
+        private final EntityUranusBossSlime slime;
+
+        public AISlimeHop(EntityUranusBossSlime slimeIn)
+        {
+            this.slime = slimeIn;
+            this.setMutexBits(5);
+        }
+
+        /**
+         * Returns whether the EntityAIBase should begin execution.
+         */
+        public boolean shouldExecute()
+        {
+            return true;
+        }
+
+        /**
+         * Keep ticking a continuous task that has already been started
+         */
+        public void updateTask()
+        {
+            ((EntityUranusBossSlime.SlimeMoveHelper)this.slime.getMoveHelper()).setSpeed(1.0D);
+        }
+    }
+
+    static class SlimeMoveHelper extends EntityMoveHelper
+    {
+        private float yRot;
+        private int jumpDelay;
+        private final EntityUranusBossSlime slime;
+        private boolean isAggressive;
+
+        public SlimeMoveHelper(EntityUranusBossSlime slimeIn)
+        {
+            super(slimeIn);
+            this.slime = slimeIn;
+            this.yRot = 180.0F * slimeIn.rotationYaw / (float)Math.PI;
+        }
+
+        public void setDirection(float p_179920_1_, boolean p_179920_2_)
+        {
+            this.yRot = p_179920_1_;
+            this.isAggressive = p_179920_2_;
+        }
+
+        public void setSpeed(double speedIn)
+        {
+            this.speed = speedIn;
+            this.action = EntityMoveHelper.Action.MOVE_TO;
+        }
+
+        public void onUpdateMoveHelper()
+        {
+            this.entity.rotationYaw = this.limitAngle(this.entity.rotationYaw, this.yRot, 90.0F);
+            this.entity.rotationYawHead = this.entity.rotationYaw;
+            this.entity.renderYawOffset = this.entity.rotationYaw;
+
+            if (this.action != EntityMoveHelper.Action.MOVE_TO)
+            {
+                this.entity.setMoveForward(0.0F);
+            }
+            else
+            {
+                this.action = EntityMoveHelper.Action.WAIT;
+
+                if (this.entity.onGround)
+                {
+                    this.entity.setAIMoveSpeed((float)(this.speed * this.entity.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).getAttributeValue()));
+
+                    if (this.jumpDelay-- <= 0)
+                    {
+                        this.jumpDelay = this.slime.getJumpDelay();
+
+                        if (this.isAggressive)
+                        {
+                            this.jumpDelay /= 3;
+                        }
+
+                        this.slime.getJumpHelper().setJumping();
+
+                        if (this.slime.makesSoundOnJump())
+                        {
+                            this.slime.playSound(this.slime.getJumpSound(), this.slime.getSoundVolume(), ((this.slime.getRNG().nextFloat() - this.slime.getRNG().nextFloat()) * 0.2F + 1.0F) * 0.8F);
+                        }
+                    }
+                    else
+                    {
+                        this.slime.moveStrafing = 0.0F;
+                        this.slime.moveForward = 0.0F;
+                        this.entity.setAIMoveSpeed(0.0F);
+                    }
+                }
+                else
+                {
+                    this.entity.setAIMoveSpeed((float)(this.speed * this.entity.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).getAttributeValue()));
+                }
+            }
+        }
     }
 
     @Override
     public int getChestTier() {
-        return 0;
-    }
-
-    @Override
-    public ItemStack getGuaranteedLoot(Random rand) {
-        return null;
+        return 7;
     }
 
     @Override
     public void dropKey() {
-
+        this.entityDropItem(new ItemStack(SolItems.KEY_TIER_7, 1), 0.5F);
     }
 
     @Override
     public BossInfo.Color getHealthBarColor() {
-        return null;
+        return BossInfo.Color.BLUE;
+    }
+
+    @Override
+    public void onKillCommand()
+    {
+        this.setHealth(0.0F);
+    }
+
+    @Override
+    public ItemStack getGuaranteedLoot(Random rand) {
+        List<ItemStack> stackList = new LinkedList<>();
+        stackList.addAll(GalacticraftRegistry.getDungeonLoot(7));
+        return stackList.get(rand.nextInt(stackList.size())).copy();
     }
 
     @Override
--- src/main/java/mod/sol/init/SolItems.java
+++ src/main/java/mod/sol/init/SolItems.java
@@ -70,6 +70,18 @@ public class SolItems
 	public static final Item KEY_TIER_6 = new ItemKeyTier6("key_t6");
 	public static final Item SCHEMATIC_T6 = new ItemSchematicTier6("schematic_rocket_t6");
 	public static final Item ROCKET_T6 = new ItemTier6Rocket("rocket_t6");
+	// t7
+	public static final Item ROCKET_ENGINE_T7 = new ItemBase("rocket_engine_t7", TheSol.ITEM_TAB);
+	public static final Item REINFORCED_PLATE_T7 = new ItemBase("reinforced_plate_t7", TheSol.ITEM_TAB);
+	public static final Item NOSE_CONE_T7 = new ItemBase("nose_cone_t7", TheSol.ITEM_TAB);
+	public static final Item ROCKET_FINS_T7 = new ItemBase("rocket_fins_t7", TheSol.ITEM_TAB);
+	public static final Item ENGINE_BOOSTER_T7 = new ItemBase("engine_booster_t7", TheSol.ITEM_TAB);
+	/*public static final Item KEY_TIER_7 = new ItemKeyTier7("key_t7");
+	public static final Item SCHEMATIC_T7 = new ItemSchematicTier7("schematic_rocket_t7");
+	public static final Item ROCKET_T7 = new ItemTier7Rocket("rocket_t7");*/
+	public static final Item KEY_TIER_7 = new ItemBase("key_t7", TheSol.ITEM_TAB);
+	public static final Item SCHEMATIC_T7 = new ItemBase("schematic_rocket_t7", TheSol.ITEM_TAB);
+	public static final Item ROCKET_T7 = new ItemBase("rocket_t7", TheSol.ITEM_TAB);
 	// t8
 	// public static final Item ROCKET_T8 = new ItemTier8Rocket("rocket_t8");
 	// battery
