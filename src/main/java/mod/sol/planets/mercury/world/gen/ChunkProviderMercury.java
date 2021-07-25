43680f6930bde375136f3be71ff252bb49fc773c
Arrokoth486958 <87651575+Arrokoth486958@users.noreply.github.com>
2021-07-25 14:20:35 +0200

    Edited Mercury chunk generation

--- src/main/java/mod/sol/planets/mercury/world/gen/ChunkProviderMercury.java
+++ src/main/java/mod/sol/planets/mercury/world/gen/ChunkProviderMercury.java
@@ -82,8 +82,8 @@ public class ChunkProviderMercury extends ChunkProviderBase
         {
             for (int z = 0; z < ChunkProviderMercury.CHUNK_SIZE_Z; z++)
             {
-                final double d = this.noiseGen1.getNoise(x + chunkX * 16, z + chunkZ * 16) * 8;
-                final double d2 = this.noiseGen2.getNoise(x + chunkX * 16, z + chunkZ * 16) * 24;
+                final double d = this.noiseGen1.getNoise(x + chunkX * 16, z + chunkZ * 16) * 32;
+                final double d2 = this.noiseGen2.getNoise(x + chunkX * 16, z + chunkZ * 16) * 36;
                 double d3 = this.noiseGen3.getNoise(x + chunkX * 16, z + chunkZ * 16) - 0.1;
                 d3 *= 4;
 
