994a24c9ea642d402e2284233bee33c5304247e3
Arrokoth486958 <87651575+Arrokoth486958@users.noreply.github.com>
2021-08-01 21:17:31 +0200

    Removed unused import

--- src/main/java/mod/sol/TheSol.java
+++ src/main/java/mod/sol/TheSol.java
@@ -77,7 +77,6 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLLoadEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -224,7 +223,7 @@ public class TheSol
 		RenderingRegistry.registerEntityRenderingHandler(EntitySaturnBossStray.class, (RenderManager manager) -> new RenderSaturnBossStray(manager));
 		RenderingRegistry.registerEntityRenderingHandler(EntityUranusBossSlime.class, (RenderManager manager) -> new RenderUranusBossSlime(manager));
 	}
-	
+
 	@EventHandler
 	public void init(FMLInitializationEvent event)
 	{
